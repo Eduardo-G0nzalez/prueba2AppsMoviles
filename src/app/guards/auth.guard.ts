890ac241ff7f2fceb3642dbcf9fe776d7d4124d7
Eduardo <134351368+Eduardo-G0nzalez@users.noreply.github.com>
2024-11-02 10:06:07 -0300

    Update auth.guard.ts

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -9,30 +9,26 @@ export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot): boolean {
-    // Comprobación de autenticación
     if (!this.authService.isAuthenticated()) {
       alert('Debe iniciar sesión para acceder a esta vista');
       this.router.navigate(['/login']);
-      return false; // Bloqueo inmediato
+      return false;
     }
 
     const userRole = this.authService.getUserRole();
 
-    // Verificación específica para el rol "docente" en la ruta 'qr'
     if (route.routeConfig?.path === 'qr' && userRole !== 'docente') {
       alert('Acceso restringido a docentes');
-      this.router.navigate(['/login']); // Redirigir al login u otra vista adecuada
-      return false; // Bloqueo inmediato
+      this.router.navigate(['/login']);
+      return false;
     }
 
-    // Verificación específica para el rol "alumno" en la ruta 'scan-qr'
     if (route.routeConfig?.path === 'scan-qr' && userRole !== 'alumno') {
       alert('Acceso restringido a alumnos');
-      this.router.navigate(['/login']); // Redirigir al login u otra vista adecuada
-      return false; // Bloqueo inmediato
+      this.router.navigate(['/login']);
+      return false;
     }
 
-    // Si las comprobaciones se pasan, permite el acceso
     return true;
   }
 }
